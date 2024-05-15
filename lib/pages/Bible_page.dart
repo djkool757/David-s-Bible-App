c4ee2f3184f018468bd00990933e02e956379255
deturner <deturner@ncsu.edu>
2024-05-14 20:27:57 -0400

    creation of nav bar and forward/backward arrows for bible page

--- lib/pages/Bible_page.dart
+++ lib/pages/Bible_page.dart
@@ -1,6 +1,8 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+class BiblePage extends StatelessWidget {
+  const BiblePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -8,8 +10,57 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bible'),
       ),
-      body: const Center(
-        child: Text('Bible Page'),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                  backgroundColor: Color.fromARGB(255, 174, 173, 173)
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.book),
+                  label: 'Bible',
+                  backgroundColor: Colors.white
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: 'Discover',
+                  backgroundColor: Color.fromARGB(255, 174, 173, 173)
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 60.0), // adjust as needed
+              child: FloatingActionButton.small(
+                onPressed: () {
+                  // go to previous page of bible
+                },
+                child: const Icon(Icons.arrow_back_ios_rounded),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 60.0), // adjust as needed
+              child: FloatingActionButton.small(
+                onPressed: () {
+                  // go to next page of bible
+                },
+                child: const Icon(Icons.arrow_forward_ios_rounded),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
